f75a311ba29eaf9a6c2ba61ba32d5d84a23e88f9
ThroughTimeAndSpaceTraveler <theelectromagneticinduction@gmail.com>
2021-10-12 19:41:37 +0300

    add new array methods

--- js_methods.js
+++ js_methods.js
@@ -98,6 +98,54 @@ const transportation = data.reduce((obj, item) => {
     obj[item]++;
     return obj;
 }, {});
-
 console.log(transportation)
 
+// *******************************************
+
+const human = [
+    { name: 'Wes', year: 1988},
+    { name: 'Kait', year: 1986},
+    { name: 'Irv', year: 1970},
+    { name: 'Lux', year: 2015},
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423},
+    { text: 'Super good', id: 823423},
+    { text: 'You are the best', id: 2039842},
+    { text: 'Ramen is my fav food ever', id: 123523},
+    { text: 'Nice Nice Nice', id: 542328},
+];
+
+// Some and every checks
+// Array/prototype.some() // is at least one person 19?
+const inAdult = human.some(person => {
+    const currentYear = (new Date()).getFullYear();
+    return currentYear - person.year >= 19;
+});
+
+
+const allAdults = human.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+// ****************************************
+
+// array.prototype.find()
+// find is like filter but instead returns just one you are looking for
+// find the comment with the ID of 823423
+
+const comment = comments.find(comment => comment.id === 823423);
+
+// *****************************************
+
+// Array.prototype.findIndex()
+// find the comment with this ID
+// delete the comment with the ID of 823423
+
+const index = comments.findIndex(comment => comment.id === 823423);
+
+// comments.splice(index, 1);
+
+const newComments = [ //i've got no idea how this works
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+];
